d5c35a3663af395d59bc4968c82c11b92feba243
meganwalley <44676998+meganwalley@users.noreply.github.com>
2021-01-13 10:11:33 -0800

    Working Movement Script

--- Assets/Scripts/PlayerMovementScript.cs
+++ Assets/Scripts/PlayerMovementScript.cs
@@ -4,15 +4,81 @@ using UnityEngine;
 
 public class PlayerMovementScript : MonoBehaviour
 {
+    new Rigidbody2D body;
+    string direction;
+    float movement = 4;
+    /*
+     height = 1;
+     left = 0.5;
+     right = 1.5;
+     height left = 0.75;
+     height right = 1.25;
+     */
+
+    bool inControl = true;
     // Start is called before the first frame update
     void Start()
     {
-        
+        body = GetComponent<Rigidbody2D>();
     }
 
     // Update is called once per frame
     void Update()
     {
         
+        if (Input.GetKey(KeyCode.W) || Input.GetKey(KeyCode.UpArrow))
+        {
+            direction = "N";
+        } else if (Input.GetKey(KeyCode.S) || Input.GetKey(KeyCode.DownArrow))
+        {
+            direction = "S";
+        } else
+        {
+            direction = "";
+        }
+
+        if (Input.GetKey(KeyCode.D) || Input.GetKey(KeyCode.RightArrow))
+        {
+            direction += "E";
+        }
+        if (Input.GetKey(KeyCode.A) || Input.GetKey(KeyCode.LeftArrow))
+        {
+            direction += "W";
+        }
     }
+
+    private void FixedUpdate()
+    {
+        switch(direction)
+        {
+            case "N":
+                body.velocity = new Vector2(0, 1F* movement);
+                break;
+            case "NE":
+                body.velocity = new Vector2(1F* movement, 0.75F * movement);
+                break;
+            case "E":
+                body.velocity = new Vector2(1.5F * movement, 0);
+                break;
+            case "SE":
+                body.velocity = new Vector2(1F * movement, -0.75F * movement);
+                break;
+            case "S":
+                body.velocity = new Vector2(0, -1F * movement);
+                break;
+            case "SW":
+                body.velocity = new Vector2(-0.75F * movement, -0.75F * movement);
+                break;
+            case "W":
+                body.velocity = new Vector2(-0.5F * movement, 0);
+                break;
+            case "NW":
+                body.velocity = new Vector2(-0.75F * movement, 0.75F * movement);
+                break;
+            default:
+                body.velocity = new Vector2(0, 0);
+                break;
+        }
+    }
+
 }
