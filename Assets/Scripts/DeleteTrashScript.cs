39b6ffce4d53a59a99c9efa8321bb8d8f0457bb5
meganwalley <44676998+meganwalley@users.noreply.github.com>
2021-01-14 11:23:41 -0800

    garbage collection and view priority is fixed/updated

--- Assets/Scripts/DeleteTrashScript.cs
+++ Assets/Scripts/DeleteTrashScript.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class DeleteTrashScript : MonoBehaviour
 {
-    private void OnCollisionEnter2D(Collision2D collision)
+    private void OnTriggerEnter2D(Collider2D collision)
     {
         Destroy(collision.gameObject);
     }
